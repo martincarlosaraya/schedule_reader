bc4b700e580a2425b6cf1a5aa55beb88ae9873ab
martincarlosaraya <maray@equinor.com>
2024-02-02 14:57:37 +0100

    added compdatl

--- src/schedule_reader/compdat.py
+++ src/schedule_reader/compdat.py
@@ -3,10 +3,10 @@ import numpy as np
 
 from .dates import parse_dates
 from .extract_keyword import extract_keyword
-from .welspecs import extract_welspecs
+from .welspec import extract_welspecs, extract_welspecl
 
 
-def _defaultIJ(well, date, IJ, welspecs_table):
+def _defaultIJ(well, date, IJ, welspec_table):
     """
     Helper function to find the I and J coordinates for a well, from its WELSPECS definition.
     Used when the I or J coordinates are defaulted in the COMPDAT keyword.
@@ -20,13 +20,13 @@ def _defaultIJ(well, date, IJ, welspecs_table):
             A DataFrame containing at least the 'date', 'well', 'I' and 'J' data from the WELSPEC keyword.
             This DataFrame is prepared by the function `extract_welspecs`. It is automatically called by the function `extract_compdat` if required.
     """
-    return welspecs_table[(welspecs_table['well'] == well) & (welspecs_table['date'] <= date), IJ].iloc[-1]
+    return welspec_table[(welspec_table['well'] == well) & (welspec_table['date'] <= date), IJ].iloc[-1]
 
 
 def extract_compdat(schedule_dict):
     """
     Shortcut for `extract_keyword` for the COMPDAT keyword.
-    Extract the COMPDAT keyword from the schedule dictionary and return a DataFrame of WELSPECS data by DATES.
+    Extract the COMPDAT keyword from the schedule dictionary and return a DataFrame of COMPDAT data by DATES.
 
     Params:
         schedule_dict: dict
@@ -57,3 +57,66 @@ def extract_compdat(schedule_dict):
     compdat_table['direction'] = compdat_table['direction'].fillna('Z').astype('category', errors='ignore')
     return compdat_table
 
+
+def extract_compdatl(schedule_dict):
+    """
+    Shortcut for `extract_keyword` for the COMPDAT keyword.
+    Extract the COMPDAT keyword from the schedule dictionary and return a DataFrame of COMPDATL data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    compdatl_columns = ['date', 'well', 'local grid', 'I', 'J', 'K_up', 'K_low', 'status', 'saturation table', 'transimissibility factor', 'well bore diameter', 'Kh', 'skin', 'D-factor', 'direction', 'pressure equivalent radius']
+    compdatl_table = extract_keyword(schedule_dict, 'COMPDAT', compdatl_columns)  # {}
+    compdatl_table['date'] = parse_dates(compdatl_table['date'].to_list())  # to parse dates exactly as stated by DATES eclipse format
+    compdatl_table['well'] = [well.strip("'") for well in compdatl_table['well']]
+    compdatl_table['well'] = compdatl_table['well'].astype('category', errors='ignore')
+    compdatl_table.replace('1*', None, inplace=True)
+    compdatl_table.replace("'1*'", None, inplace=True)
+    to_int = ['I', 'J', 'K_up', 'K_low']
+    compdatl_table[to_int] = compdatl_table[to_int].fillna('0').astype(int, errors='ignore')
+    if 0 in compdatl_table['I'].values or 0 in compdatl_table['J'].values:
+        welspecl_table = extract_welspecl(schedule_dict)
+        if 0 in compdatl_table['I'].values:
+            compdatl_table['I'] = [compdatl_table['I'].iloc[r] if compdatl_table['I'].iloc[r] > 0 else _defaultIJ(compdatl_table['date'].iloc[r], compdatl_table['date'].iloc[r], 'I', welspecl_table) for r in len(compdatl_table)]
+        if 0 in compdatl_table['J'].values:
+            compdatl_table['J'] = [compdatl_table['I'].iloc[r] if compdatl_table['I'].iloc[r] > 0 else _defaultIJ(compdatl_table['date'].iloc[r], compdatl_table['date'].iloc[r], 'J', welspecl_table) for r in len(compdatl_table)]
+    to_float = ['transimissibility factor', 'well bore diameter', 'Kh', 'skin', 'D-factor', 'pressure equivalent radius']
+    compdatl_table[to_float] = compdatl_table[to_float].astype(float, errors='ignore')
+    compdatl_table['status'] = compdatl_table['status'].fillna('OPEN').astype('category', errors='ignore')
+    compdatl_table['skin'].fillna(0.0, inplace=True)
+    compdatl_table['direction'] = compdatl_table['direction'].fillna('Z').astype('category', errors='ignore')
+    return compdatl_table
+
+
+def extract_compdatm(schedule_dict):
+    """
+    alias for extract_compdatl
+    """
+    return extract_compdatl(schedule_dict)
+
+
+def extract_compdat2(schedule_dict):
+    """
+    Extract COMPDAT, COMPDATL and COMPDATM from the schedule dictionary and return a DataFrame of COMPDAT(s) data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    compdat = extract_compdat(schedule_dict)
+    compdatl = extract_compdatl(schedule_dict)
+
+    if len(compdat) > 0 and len(compdatl) > 0:
+        return pd.concat([compdat, compdatl], axis=0, ignore_index=False)
+    elif len(compdatl) > 0:
+        return compdatl
+    else:
+        return compdat
