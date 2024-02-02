c46187cb54ace99d83e9f26ac1667975f3bafb2b
martincarlosaraya <maray@equinor.com>
2024-02-02 14:58:09 +0100

    wconprod, wconinje and history keywrods

---
+++ src/schedule_reader/wcon.py
@@ -0,0 +1,101 @@
+import pandas as pd
+from .dates import parse_dates
+from .extract_keyword import extract_keyword
+
+
+def extract_wconprod(schedule_dict):
+    """
+    Shortcut for `extract_keyword` for the WCONPROD keyword.
+    Extract the WCONPROD keyword from the schedule dictionary and return a DataFrame of WELSPECS data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    wconprod_columns = ['date', 'well', 'status', 'control mode', 'OIL rate', 'WATER rate', 'GAS rate', 'LIQUID rate', 'RESERVOIR fluid rate', 
+                        'BHP limit', 'THP limit', 'VFP', 'ALQ',
+                        'wet gas rate', 'total molar rate', 'steam rate',
+                        'pressure offset', 'temperature offset', 'calorific target rate', 'linearly combined rate', 'NGL rate'] 
+    wconprod_table = extract_keyword(schedule_dict, 'WCONPROD', wconprod_columns)  # {}
+    wconprod_table['date'] = parse_dates(wconprod_table['date'].to_list())  # to parse dates exactly as stated by DATES eclipse format
+    wconprod_table['well'] = [well.strip("'") for well in wconprod_table['well']]
+    wconprod_table['well'] = wconprod_table['well'].astype('category')
+    wconprod_table.replace('1*', None, inplace=True)
+    wconprod_table.replace("'1*'", None, inplace=True)
+    wconprod_table[['I', 'J']] = wconprod_table[['I', 'J']].astype(int)
+    return wconprod_table
+
+
+def extract_wconinje(schedule_dict):
+    """
+    Shortcut for `extract_keyword` for the WCONINJE keyword.
+    Extract the WCONINJE keyword from the schedule dictionary and return a DataFrame of WELSPECS data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    wconinje_columns = ['date', 'well', 'injector type', 'status', 'control mode', 'SURFACE fluid rate', 'RESERVOIR fluid rate', 'BHP limit', 'THP limit', 'VFP',
+                        'vap oil concentration', 'thermal ratio of gas to steam', 'OIL proportion', 'WATER proportion', 'GAS proportion', 'ratio of oil oil to steam'] 
+    wconinje_table = extract_keyword(schedule_dict, 'WCONINJE', wconinje_columns)  # {}
+    wconinje_table['date'] = parse_dates(wconinje_table['date'].to_list())  # to parse dates exactly as stated by DATES eclipse format
+    wconinje_table['well'] = [well.strip("'") for well in wconinje_table['well']]
+    wconinje_table['well'] = wconinje_table['well'].astype('category')
+    wconinje_table.replace('1*', None, inplace=True)
+    wconinje_table.replace("'1*'", None, inplace=True)
+    wconinje_table[['I', 'J']] = wconinje_table[['I', 'J']].astype(int)
+    return wconinje_table
+
+
+def extract_wconhist(schedule_dict):
+    """
+    Shortcut for `extract_keyword` for the WCONHIST keyword.
+    Extract the WCONHIST keyword from the schedule dictionary and return a DataFrame of WELSPECS data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    wconhist_columns = ['date', 'well', 'status', 'control mode', 'OIL rate', 'WATER rate', 'GAS rate', 'VFP', 'ALQ',
+                        'THP limit', 'BHP limit', 'wet gas rate', 'NGL rate']
+    wconhist_table = extract_keyword(schedule_dict, 'WCONHIST', wconhist_columns)  # {}
+    wconhist_table['date'] = parse_dates(wconhist_table['date'].to_list())  # to parse dates exactly as stated by DATES eclipse format
+    wconhist_table['well'] = [well.strip("'") for well in wconhist_table['well']]
+    wconhist_table['well'] = wconhist_table['well'].astype('category')
+    wconhist_table.replace('1*', None, inplace=True)
+    wconhist_table.replace("'1*'", None, inplace=True)
+    wconhist_table[['I', 'J']] = wconhist_table[['I', 'J']].astype(int)
+    return wconhist_table
+
+
+def extract_wconinjh(schedule_dict):
+    """
+    Shortcut for `extract_keyword` for the WCONINJE keyword.
+    Extract the WCONINJE keyword from the schedule dictionary and return a DataFrame of WELSPECS data by DATES.
+
+    Params:
+        schedule_dict: dict
+            shedule dictionary prepared by the .data_reader.read_data function
+    
+    Return:
+        pandas.DataFrame
+    """
+    wconinjh_columns = ['date', 'well', 'injector type', 'status', 'injection rate', 'BHP', 'THP', 'VFP',
+                        'vap oil concentration', 'OIL proportion', 'WATER proportion', 'GAS proportion', 'control model']
+    wconinjh_table = extract_keyword(schedule_dict, 'WCONINJE', wconinjh_columns)  # {}
+    wconinjh_table['date'] = parse_dates(wconinjh_table['date'].to_list())  # to parse dates exactly as stated by DATES eclipse format
+    wconinjh_table['well'] = [well.strip("'") for well in wconinjh_table['well']]
+    wconinjh_table['well'] = wconinjh_table['well'].astype('category')
+    wconinjh_table.replace('1*', None, inplace=True)
+    wconinjh_table.replace("'1*'", None, inplace=True)
+    wconinjh_table[['I', 'J']] = wconinjh_table[['I', 'J']].astype(int)
+    return wconinjh_table
